6c107b130ee6a7e20f36b8b044a760b0ad0c1b91
fc2115 <felixcrowther@me.com>
2019-01-10 11:33:07 +0000

    Update twitter_credentials.py

--- Auto_sense_store/twitter_credentials.py
+++ Auto_sense_store/twitter_credentials.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Variables to access twitter API
-ACCESS_TOKEN = "1734453108-eE4kXUoRH3cPowmrhgrDUb0MF5cmdLrOAACgm7v"
-ACCESS_TOKEN_SECRET = "hePmjTvmQBh6KADfAZifVY8os2zBzOoouvkxNc2Xh9mza"
-CONSUMER_KEY = "AG0J3kMEY4yBYcZX9maxPqGRb"
-CONSUMER_SECRET = "rbq3shRL66tiRGOkbggDn2mXj26cINbPn2hLpgOylP60nnuafz"
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
